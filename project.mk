d57a20ce217553608ef864a9ca339db04c08e4b4
sadko4u <sadko4u@gmail.com>
2022-09-06 19:47:45 +0300

    Preparing for release

--- project.mk
+++ project.mk
@@ -24,7 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-spectrum-analyzer
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Spectrum Analyzer Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.8-devel
+ARTIFACT_VERSION            = 1.0.8
 
 
 
