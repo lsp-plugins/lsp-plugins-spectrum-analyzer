0b17438130418cb35a7158a1165e607945e6b3a3
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-01-03 19:49:37 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -24,6 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-spectrum-analyzer
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Spectrum Analyzer Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
+ARTIFACT_EXPORT_HEADERS     = 0
 ARTIFACT_VERSION            = 1.0.11-devel
 
 
